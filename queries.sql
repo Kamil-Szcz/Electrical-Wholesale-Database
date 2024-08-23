95c675e9329ad05e95e0341a5b455124fba0738c
Kamil <152660421+Kamil-Szcz@users.noreply.github.com>
2024-08-23 20:13:00 +0200

    Add files via upload

---
+++ queries.sql
@@ -0,0 +1,32 @@
+-- 1. Which products are sold most often?
+SELECT p.product_id, SUM(si.quantity) AS sales_total
+FROM products p
+INNER JOIN sales_items si
+ON p.product_id = si.product_id
+GROUP BY p.product_id
+ORDER BY sales_total DESC;
+
+-- 2. What are the sales trends by month/days?
+-- by month
+SELECT SUM(si.quantity) AS sales_total, TO_CHAR(sh.sale_date, 'YYYY-MM') AS month
+FROM sales_items si
+INNER JOIN sales_history sh
+ON si.sales_id = sh.sales_id
+GROUP BY TO_CHAR(sh.sale_date, 'YYYY-MM')
+ORDER BY month;
+
+-- by day
+SELECT SUM(si.quantity) AS sales_total, TO_CHAR(sh.sale_date, 'YYYY-MM-DD') AS day
+FROM sales_items si
+INNER JOIN sales_history sh
+ON si.sales_id = sh.sales_id
+GROUP BY TO_CHAR(sh.sale_date, 'YYYY-MM-DD')
+ORDER BY day;
+
+-- 3. Które dni tygodnia generuj¹ najwy¿sze obroty?
+SELECT SUM(si.quantity) AS sales_total, TO_CHAR(sh.sale_date, 'Day', 'NLS_DATE_LANGUAGE=Polish') AS day_of_week
+FROM sales_items si
+INNER JOIN sales_history sh
+ON si.sales_id = sh.sales_id
+GROUP BY TO_CHAR(sh.sale_date, 'Day', 'NLS_DATE_LANGUAGE=Polish'), TO_CHAR(sh.sale_date, 'D', 'NLS_DATE_LANGUAGE=Polish')
+ORDER BY TO_CHAR(sh.sale_date, 'D', 'NLS_DATE_LANGUAGE=Polish'); 
