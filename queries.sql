5a8c51fbaeda5d6dde25b253245687a5916635e1
Kamil <152660421+Kamil-Szcz@users.noreply.github.com>
2024-08-24 11:15:16 +0200

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -32,3 +32,21 @@ INNER JOIN products p
 ON si.product_id = p.product_id
 GROUP BY TO_CHAR(sh.sale_date, 'Day', 'NLS_DATE_LANGUAGE=Polish')
 ORDER BY sales_value DESC;
+
+-- 4. Which customers generate the most revenue?
+CREATE VIEW CustomerPurchaseValues AS
+SELECT c.customer_name, SUM(si.quantity * p.net_sale_price) AS purchase_value
+FROM  customers c
+INNER JOIN sales_history sh 
+ON c.customer_id = sh.customer_id
+INNER JOIN sales_items si 
+ON sh.sales_id = si.sales_id
+INNER JOIN products p 
+ON si.product_id = p.product_id
+GROUP BY c.customer_name;
+
+SELECT customer_name, ROUND(purchase_value, 2) AS purchase_value 
+FROM CustomerPurchaseValues
+ORDER BY purchase_value DESC;
+
+
